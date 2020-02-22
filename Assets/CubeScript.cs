42de8c65e50e38a099707f27f9199abbb0e37ef2
User8956 <11wertgh77@mail.ru>
2020-02-22 14:38:21 +0300

    second

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public Vector3 RottationVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Rotate(RottationVector);
+    }
+}
